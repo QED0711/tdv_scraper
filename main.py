073b69825ffd9b7fa445b2d11b3bf1d7e0382431
QED0711 <qdizon@gmail.com>
2023-09-12 05:46:32 -0400

    added status messages via ntfy

--- main.py
+++ main.py
@@ -1,34 +1,49 @@
 #!/usr/bin/python
-import time, pdb
+import os, time, datetime, pdb
 from classes.browser_interface import BrowserInterface
 from classes.errors import WatchlistNotFoundError
 from environment import HEADLESS_SELENIUM
 DRIVER_PATH = '/usr/lib/chromium-browser/chromedriver'
+from keys import ntfy_url
 
 def main():
     ##################################
     ##### SELENIUM BROWSER SETUP #####
     ##################################
-    browser_options = [
-            "--no-sandbox", 
-            "--disable-dev-shm-usage", 
-            "--remote-debugging-port=9222", 
-            "--user-data-dir=/app/browser_data",
-            "--ignore-certificate-errors",
-            "--allow-insecure-localhost",
-            "--no-gpu",
-        ]
+    try:
+        browser_options = [
+                "--no-sandbox", 
+                "--disable-dev-shm-usage", 
+                "--remote-debugging-port=9222", 
+                "--user-data-dir=/app/browser_data",
+                "--ignore-certificate-errors",
+                "--allow-insecure-localhost",
+                "--no-gpu",
+            ]
 
-    if HEADLESS_SELENIUM:
-        browser_options.append("--headless")
+        if HEADLESS_SELENIUM:
+            browser_options.append("--headless")
 
-    browser = BrowserInterface(
-        option_args=browser_options,
-        js_snippets=["/app/javascript/socketListener.js"],
-    )
+        browser = BrowserInterface(
+            option_args=browser_options,
+            js_snippets=["/app/javascript/socketListener.js"],
+        )
 
-    browser.open("https://www.tradingview.com/chart")
-    watchlist_attempts = 0
+        browser.open("https://www.tradingview.com/chart")
+        time.sleep(7) # give some time for the page to load
+        browser.screenshot(f"/app/screenshots/{str(datetime.date.today())}.png")
+        watchlist_attempts = 0
+    except Exception as e:
+        os.system(f"""
+            curl \
+            -H "tdv scraper up" \
+            -H "Priority: default" \
+            -H "Tags: white_check_mark" \
+            -d "tdv scraper status: healthy" \
+            {ntfy_url}
+        """)
+    
+    
 
     #########################
     ##### GET WATCHLIST #####
